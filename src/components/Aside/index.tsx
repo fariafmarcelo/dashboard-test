00b0b4c6fde63d3f36f1e594f07fdcf5955f3fa5
Marcelo Faria <marcelo.faria@bigdatacorp.com.br>
2022-02-11 15:49:59 -0300

    feat: add react-icons and increment Aside component

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+    Container,
+    Header,
+    Title,
+    LogImg,
+    MenuContainer,
+    MenuItemLink,
+} from './styles';
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md';
+
+import LogoImg from '../../assets/logo.svg';
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={LogoImg} alt="Logo Minha Dashboard" />
+                <Title>Minha Dashboard</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -5,4 +5,48 @@ export const Container = styled.div`
 
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+    border-right: 1px solid ${props => props.theme.colors.gray};
+
+    padding-left: 20px;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    height: 70px;
+`;
+
+export const Title = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+`;
+
+export const LogImg = styled.img`
+    height: 40px;
+    width: 40px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+`;
+
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    transition: opacity ease .3s;
+    display: flex;
+    align-items: center;
+
+    margin: 7px 0;
+
+    &:hover {
+        opacity: .7;
+    }
+
+    > svg {
+        font-size: 18px;
+        margin-right: 5px;
+    }
 `;
