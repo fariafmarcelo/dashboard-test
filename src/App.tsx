cd6f14a6b75916d262cd45f63b75a9ad656ae676
Marcelo Faria <marcelo.faria@bigdatacorp.com.br>
2022-02-11 14:21:50 -0300

    feat: create default colors theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './components/Layout';
+import Dark from './styles/themes/dark';
+import Light from './styles/themes/light';
 
 const App: React.FC = () => {
     return (
-        <>
+        <ThemeProvider theme={Dark}>
             <GlobalStyles />
             <Layout />
-        </>
+        </ThemeProvider>
     );
 }
 
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#DCDCDC',
+        secondary: '#FFF',
+        tertiary: '#F5F5F5',
+        
+        white: '#000',
+        black: '#FFF',
+        gray: '#BFBFBF',
+
+        success: '#03BB85',
+        info: '#3B5998',
+        warning: '#FF6981',
+    },
+};
